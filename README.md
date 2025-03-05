eec78e19e48cfc71c4c034afe3ec868594d0bbfc
rajeebkm <rkm2541997@gmail.com>
2025-03-05 16:08:14 +0530

    arcjet protection added

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - Ref Link: https://www.youtube.com/watch?v=rOpEN1JDaD0
 - API Testing Tool: https://httpie.io/app
+- Arcjet - https://arcjet.com/
--- app.js
+++ app.js
@@ -6,11 +6,13 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import connectDB from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
